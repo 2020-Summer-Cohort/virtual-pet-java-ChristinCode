460613dc6d5c33eb47364db4691770125bc1afda
Christin Banerjee <Christinprice5@gmail.com>
2020-05-15 13:27:16 -0400

    test

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -1,4 +1,34 @@
 package virtual_pet;
 
 public class VirtualPet {
+    public void virtualSalem(String[] args) {
+
+    }
+    public void feedSalem(){
+
+    }
+
+    public void playWithSalem(){
+
+    }
+    public void cleanHairballSalem(){
+
+    }
+    public void cleanPooSalem(){
+
+    }
+    public void petSalem(){
+
+    }
+    public void cuddleSalem(){
+
+    }
+    public void brushSalem(){
+
+    }
+    public void sickSalem(){
+
+    }
+
+
 }
--- src/test/java/virtual_pet/VirtualPetTest.java
+++ src/test/java/virtual_pet/VirtualPetTest.java
@@ -1,3 +1,18 @@
 package virtual_pet;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class VirtualPetTest {
+    @Test
+    public void secondVirtualPetClass() {VirtualPetApplication test = new VirtualPetApplication();
+    }
+
+    @Test
+    public void VirtualPetApplication() {
+        VirtualPetApplication Test = new VirtualPetApplication();
+        String answer = Test.say();
+        assertEquals("Feed Salem", answer);
+    }
 }
