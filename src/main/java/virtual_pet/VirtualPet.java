0b831978e916e22d64adee65dd1a1ecacf811f75
Christin Banerjee <Christinprice5@gmail.com>
2020-05-17 19:22:39 -0400

    Requirements complete-Still need to remove notes

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -1,9 +1,12 @@
 package virtual_pet;
 
+import java.util.Random;
+
 //This whole page is about the users interactions with the game. Think of this class as a storage area: ie library for my package
 // hunger is attribute. feed is method
 //void tick is the Method
 public class VirtualPet {
+
     //Add Variable-this is the pet name we can assign name later or pet is hunger assign feed in thee update method
     private String petName;
     private String species;
@@ -13,6 +16,20 @@ public class VirtualPet {
     private int tickCount;
 
     //Get-methods first bc they are easier and cleaner.
+    public String defineSelection(int selection) {
+        if (selection == 1) {
+            feed(6);
+            return "eat";
+        } else if (selection == 2) {
+            play(5);
+            return "play";
+        } else if (selection == 3) {
+            sleep(7);
+            return "sleep";
+        }
+        return "";
+    }
+
     public String getPetName() {
         return petName;
     }
@@ -37,38 +54,87 @@ public class VirtualPet {
         return tickCount;
     }
 
-    //Update Methods are what happens to the values as user interacts ie hunger becomes
+    //Update Methods
+    //are what happens to the values as user interacts ie hunger becomes
     // do not need name or species bc they will be assigned and user does not get to name
-    //if I want user to name, I would create a public void updateName || public void updateSpecies
-
-    public void feed() {
+    //if I want user to name, I would create a public void updateName || public void updateSpecie
+    private void feed(int fed) {
+        if (hunger - fed >= 0) {
+            hunger = hunger - fed;
+        } else {
+            hunger = 0;
+        }
+        if (boredom + 2 <= 10) {
+            boredom = boredom + 2;
+        } else {
+            boredom = 10;
+        }
     }
 
-    public void Play() {
+    private void play(int activity) {
+        if (boredom - activity >= 0) {
+            boredom = boredom - activity;
+        } else {
+            boredom = 0;
+        }
+        if (tiredness + 3 <= 10) {
+            tiredness = tiredness + 3;
+        } else {
+            tiredness = 10;
+        }
     }
 
-    public void Sleep() {
+    private void sleep(int nap) {
+        if (tiredness - nap >= 0) {
+            tiredness = tiredness - nap;
+        } else {
+            tiredness = 0;
+        }
+        if (hunger + 2 <= 10) {
+            hunger = hunger + 2;
+        } else {
+            hunger = 10;
+        }
     }
 
     public void tick() { /*Tick is counting the number of time the user is interacting
      * Tick is tracking the number of times the user interacts with game loop
      * needs to be the if else */
         tickCount++;
+
+        if (hunger + 1 <= 10) {
+            hunger = hunger + 1;
+        } else {
+            hunger = 10;
+        }
+        if (boredom + 1 <= 10) {
+            boredom = boredom + 1;
+        } else {
+            boredom = 10;
+        }
+        if (tiredness + 2 <= 10) {
+            tiredness = tiredness + 2;
+        } else {
+            tiredness = 10;
+        }
     }
 
     //Constructor
     //this is the Dam Examples
-    public VirtualPet(String animalName, String animalSpecies, int initialHunger,
-                      int initialBoredom, int initialTiredness) {
+    public VirtualPet(String animalName, String animalSpecies) {
+        Random generateNumber = new Random();/*declaration-declaring random object*/
+        int numberGenerated;/*declaration-declaring random object*/
 
-
-        this.petName = animalName;
+        this.petName = animalName; /*from here below, I am initializing the variables*/
         this.species = animalSpecies;
-        this.hunger = initialHunger;
-        this.boredom = initialBoredom;
-        this.tiredness = initialTiredness;
-    }
 
+        numberGenerated = generateNumber.nextInt(10);
+        this.hunger = numberGenerated;
+        numberGenerated = generateNumber.nextInt(10);
+        this.boredom = numberGenerated;
+        numberGenerated = generateNumber.nextInt(10);
+        this.tiredness = numberGenerated;
+    }
 }
 
 
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -1,21 +1,33 @@
 package virtual_pet;
 
-import org.w3c.dom.ls.LSOutput;
-
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class VirtualPetApplication {
 
     public static void main(String[] args) {
 //will have a while loop here. while(true) will make an endless loop.
+        VirtualPet pet = new VirtualPet("Salem", "cat");
+        Scanner activeUser = new Scanner(System.in);
+        int usersSelection = 1;
 
-        System.out.println("Hello, Salem is excited to see you. He is currently not doing well and needs attention.");
-        System.out.println(Replace with Salems current stats);
-        System.out.println( prompt the user to choose 1-3);
-        Scanner userPickNumber; new Scanner(System.in);
-        System.out.println( "You "+ tbd+ " Salem. What would you like t do next?");
-        //add get help feature for numbers guest outside of 1-3
+        System.out.println("Hello, " + pet.getPetName() + ", the cuddly " + pet.getSpecies() + ", is excited to see you. He is currently not doing well and needs attention.");
 
+        while (usersSelection != 0) {
+            System.out.println(pet.getPetName() + "'s need levels are:");
+            System.out.println("Hunger: " + pet.getHunger());
+            System.out.println("Boredom: " + pet.getBoredom());
+            System.out.println("Tiredness: " + pet.getTiredness());
+            System.out.println("What would you like to do?\n" + "Press 1 to feed\n" + "Press 2 to play\n" + "Press 3 to sleep\n" + "Press 0 to end game");
+
+            usersSelection = activeUser.nextInt();
+
+            pet.tick();
+
+            if (usersSelection != 0) {
+                System.out.println("You chose for " + pet.getPetName() + " to " + pet.defineSelection(usersSelection) + ". What would you like to do next?");
+            }
+            //add get help feature for numbers guest outside of 1-3
+        }
+        System.out.println("Thank you for taking care of " + pet.getPetName() + ". See you next time!");
     }
-    }
+}
